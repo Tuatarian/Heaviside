47234e8bff0ba789ea5eb1316b174f641f41182f
Tuatarian <betterthangeowizard@gmail.com>
2022-09-23 18:22:40 -0700

    functional tokenization

--- lexer.nim
+++ lexer.nim
@@ -1,12 +1,12 @@
-import os, strutils, sequtils, sugar
+import os, strutils, sequtils, sugar, zero_functional
 
 const syms = readFile("./symbols.txt").splitLines
 
 const lexStop = @[',', '\n'] & syms[1].split(',').toSeq.map(x => x[0])
-echo lexStop
+const whitespaces = syms[2].split(',').toSeq.map(x => x[0])
 
 type TKind = enum
-    TkNumLit, TkIdent, TkWSpace
+    TkNumLit, TkIdent, TkWSpace, TkStrLit
 
 type NKind = enum
     NkIdent, NkCall
@@ -20,11 +20,13 @@ type ASTNode = object
     name : string
     left, right : int
 
-func `$`(n : ASTNode) : string = n.name
+func `!`(n : ASTNode) : string = n.name
+func `!`(n : Token) : string = n.val
 
-func `$$`(n : ASTNode : char = n.name[0]
+func `$$`(n : ASTNode) : char = n.name[0]
+func `$$`(n : Token) : char = n.val[0]
 
-let inp = readFile(commandLineParams()[0])
+let inp = readFile(commandLineParams()[0]).splitLines[4]
 echo inp
 
 proc partFile(inp : string) : seq[string] =
@@ -33,38 +35,38 @@ proc partFile(inp : string) : seq[string] =
         if c in lexStop:
             if cWord.len != 0:
                 result.add cWord
-            result.add $$c
+            result.add $c
             cWord = ""
         else: cWord.add c
 
 proc tokenize(inp : seq[string]) : seq[Token] =
     for i in 0..<inp.len:
-        case inp[i]:
-            of inp[i] in [' ', '\n']:
-                result.add Token(kind : TkWSpace, val : inp[i])
-            of inp[i][0] in '0'..'9':
-                result.add Token(kind : TkNumLit, val : inp[i])
-            of inp[i] in lexStop
+        if inp[i][0] in '0'..'9':
+            result.add Token(kind : TkNumLit, val : inp[i])
+        elif inp[i][0] == '\"' and inp[i][^1] == '\"':
+            result.add Token(kind : TkStrLit, val : inp[i])
+        elif inp[i][0] notin whitespaces:
+            result.add Token(kind : TkIdent, val : inp[i])
 
-func parseCall(lex : seq[ASTNode], beg : int, inx : var int) : seq[(nType, string)] =
-    # Assume beg opens at the call
-    # We remove exactly one node from the beginning (paren)
-    if $$lex[beg - 1] notin lexStop and $$lex[beg - 1] != ' ':
-        result.add ASTNode(kind : Call, name : $lex[beg - 1])
-    var i = beg + 1
-    while $$lex[i] != ')':
-        if $$lex[i] == '(':
-            var enx : int
-            result.add lex.parseCall(i, enx)
-            result[^1].left = beg - 1
-        if $$lex[i + 1] == ',':
-            result.add ASTNode(kind : Ident, name : $lex[i])
-        lex[i].left = 0
-
-
-
-func findCalls(lex : var seq[ASTNode], start : int) : seq[ASTNode] =
-    # Assume start opens at a paren, then we should get a paren then args
+# func parseCall(lex : seq[ASTNode], beg : int, inx : var int) : seq[(nType, string)] =
+#     # Assume beg opens at the call
+#     # We remove exactly one node from the beginning (paren)
+#     if $$lex[beg - 1] notin lexStop and $$lex[beg - 1] != ' ':
+#         result.add ASTNode(kind : Call, name : $lex[beg - 1])
+#     var i = beg + 1
+#     while $$lex[i] != ')':
+#         if $$lex[i] == '(':
+#             var enx : int
+#             result.add lex.parseCall(i, enx)
+#             result[^1].left = beg - 1
+#         if $$lex[i + 1] == ',':
+#             result.add ASTNode(kind : Ident, name : $lex[i])
+#         lex[i].left = 0
 
 
-echo partFile(inp)
+
+# func findCalls(lex : var seq[ASTNode], start : int) : seq[ASTNode] =
+    # Assume start opens at a paren, then we should get a paren then
+echo "-"
+echo inp.partFile
+echo inp.partFile.tokenize().map(x => !x)
