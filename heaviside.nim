dab322a57553aba43ccc05c319a4ef8c315a6095
Tuatarian <betterthangeowizard@gmail.com>
2022-12-08 17:23:46 -0700

    rudimentary checking for term similarity

--- heaviside.nim
+++ heaviside.nim
@@ -1,4 +1,4 @@
-import strutils, sequtils, tables, zero_functional, strformat, parlex, macros
+import strutils, sequtils, tables, zero_functional, strformat, parlex, macros, sugar, typeinfo
 
 ## We're using an object variant here since I (obviously) don't want to make the user decide whether a number should be a float or an int in a CAS
 ## Also, we're going to require significantly different handling for ints than floats, for example 3/4 should not be converted to 0.75
@@ -65,9 +65,41 @@ func `$`(v : HvVal) : string =
 proc print(v : HvVal) =
     case v.kind:
     of Num: echo v.nVal
-    of Expr: print v.eVal
+    of Expr: print(v.eVal, 0)
+
+func makeExpr(a : HvNum, p : ASTNode = nil) : HvExpr = 
+    if a.isInt:
+        return HvExpr(kind : NkIntLit, val : $a, parentalUnit : p)
+    else:
+        return HvExpr(kind : NkFloatLit, val : $a, parentalUnit : p)
+
+func `===`(e, e1 : HvExpr) : bool = ## Strict and stupid equality, not the same as checking if an expression is equal to another
+    if e.kind == e1.kind and e.val == e1.val and e.kids.len == e1.kids.len:
+        for i in 0..<e.kids.len:
+            if not (e[i] === e1[i]):
+                return false
+        return true
+    return false
 
 #---------Tree Transformation Functions---------#
+
+func likeTerms(e, e1 : HvExpr) : bool =
+    if e.kind == NkCall and e1.kind == NkCall and !e == "*" and !e1 == "*":
+        var terms : seq[HvExpr]
+        for kid in e.kids:
+            if kid.kind == NkIdent:
+                terms.add kid
+
+        for kid in e1.kids:
+            if kid.kind == NkIdent:
+                if terms.len == 0: return false
+                for i in 0..<terms.len:
+                    if terms[i] === kid:
+                        terms.del(i)
+                        break
+                    if i == terms.len - 1: return false
+
+        return terms.len == 0
 
 #-------HvFuncs-------#
 # We'll prefix the versions of functions used by heaviside with hv to avoid confusion, so `/` (division) will be hvDiv, etc
@@ -75,144 +107,71 @@ proc print(v : HvVal) =
 # Also, to make my life easier, we'll overload everything so that I can use the same syntax for casting down the line
 
 # Perhaps the sum function should be different, maybe variadic, so I'll call the `+` operator as hvPlus for now
-func hvPlus(a, b : HvNum) : HvNum =
+func hvPlus(a, b : HvNum) : HvExpr =
     if a.isInt and b.isInt:
-        return makenum(a.iVal + b.iVal)
+        return makeExpr makenum(a.iVal + b.iVal)
     elif a.isInt:
-        return makenum(b.fVal + a.iVal.float)
+        return makeExpr makenum(b.fVal + a.iVal.float)
     elif b.isInt:
-        return makenum(b.iVal.float + a.fVal)
-    return makenum(b.fVal + a.fVal)
+        return makeExpr makenum(b.iVal.float + a.fVal)
+    return makeExpr makenum(b.fVal + a.fVal)
 # This doesn't handle exprs, we'll need to do that differently
 
-func hvMinus(a, b : HvNum) : HvNum = hvPlus(a, -b)
+func hvMinus(a, b : HvNum) : HvExpr {.inline.} = hvPlus(a, -b)
 
-func hvTimes(a, b : HvNum) : HvNum =
+func hvTimes(a, b : HvNum) : HvExpr =
     if a.isInt and b.isInt:
-        return makenum(a.iVal * b.iVal)
+        return makeExpr makenum(a.iVal * b.iVal)
     elif a.isInt:
-        return makenum(a.iVal.float * b.fVal)
+        return makeExpr makenum(a.iVal.float * b.fVal)
     elif b.isInt:
-        return makenum(a.fVal * a.iVal.float)
-    return makenum(a.fVal * b.fVal)
+        return makeExpr makenum(a.fVal * a.iVal.float)
+    return makeExpr makenum(a.fVal * b.fVal)
+
+func hvTimes(e : HvExpr, a : HvNum) : HvExpr =
+    result = HvExpr(kind : NkCall, val : "*")
+    result.add makeExpr(a, result)
+    e.reparentTo result
+
+func hvTimes(a : HvNum, e : HvExpr) : HvExpr = hvTimes(e, a) # multiplication is commutative
+
+func hvTimes(e, e1 : HvExpr) : HvExpr =
+    result = HvExpr(kind : NkCall, val : "*")
+    reparentTo(e, e1, result)
+
+func hvPlus(e, e1 : HvExpr) : HvExpr =
+    result = HvExpr(kind : NkCall, val : "+")
+    reparentTo(e, e1, result)
+
+func hvPlus(a : HvNum, e : HvExpr) : HvExpr =
+    result = HvExpr(kind : NkCall, val : "+")
+    result.add makeExpr(a, result)
+    e.reparentTo result
+
+func hvPlus(e : HvExpr, a : HvNum) : HvExpr {.inline.} = hvPlus(a, e) # addition is commutative
 
 #-------Building the Function Table-------#
 
-# We're (probably) gonna need 3 tables
-# These are all also going to be comptime. I will have to resolve user defined functions in some other way
-
-# macro bldFuncTable(t : typed, s : static[string]) : untyped = ## This doesn't work with generics (can extend if needed, but for now I'll just avoid using generic in hvFuncs)
-#     result = newNimNode(nnkPrefix)
-#     result.add ident("@")
-#     result.add newNimNode(nnkBracket)
-#     case t.kind
-#     of nnkSym:
-#         let res = repr t.getType[1]
-#         var argL : seq[NimNode]
-#         var args : string
-        
-#         for i in 2..<t.getType.len:
-#             args &= &"{repr t.getType[i]} "
-#             argL.add(newIdentDefs(ident("a" & $(i - 2)), ident(repr t.getType[i]), newEmptyNode()))
-
-#         result[1].add newNimNode(nnkTupleConstr).add(
-#             newLit(&"{s} {args}"), newNimNode(nnkCommand).add(
-#                 ident("pointer"), newNimNode(nnkPar).add(
-#                     newNimNode(nnkCall).add(
-#                         newNimNode(nnkPar).add(
-#                             newNimNode(nnkProcTy).add(
-#                                 newNimNode(nnkFormalParams).add(
-#                                     ident(res))))))))
-#         for arg in argL:
-#             result[1][0][1][1][0][0][0][0].add arg
-        
-#         result[1][0][1][1][0][0][0].add newNimNode(nnkPragma).add ident("nimcall")
-#         result[1][0][1][1][0].add ident(repr t)
-
-#     of nnkOpenSymChoice, nnkClosedSymChoice:
-#         var counter : int
-#         for x in t:
-#             let res = repr x.getType[1]
-#             var args : string
-
-#             for i in 2..<x.getType.len:
-#                     args &= &"{repr x.getType[i]} "
-
-#             result[1].add newNimNode(nnkTupleConstr).add(
-#                 newLit(&"{s} {args}"), newNimNode(nnkCommand).add(
-#                     ident("pointer"), newNimNode(nnkPar).add(
-#                         newNimNode(nnkCall).add(
-#                             newNimNode(nnkPar).add(
-#                                 newNimNode(nnkProcTy).add(
-#                                     newNimNode(nnkFormalParams).add(
-#                                         ident(res))))))))
-            
-#             result[1][counter][1][1][0][0][0].add newNimNode(nnkPragma).add ident("nimcall")
-#             result[1][counter][1][1][0].add ident(repr t)
-
-#             counter += 1
-#     else:
-#         error("Did not get a proc, probably a bug", t)
-
-# macro bldRetTable(t : typed, s : static[string]) : untyped =
-#     result = newNimNode(nnkPrefix)
-#     result.add ident("@")
-#     result.add newNimNode(nnkBracket)
-#     case t.kind
-#     of nnkSym:
-#         let res = repr t.getType[1]
-#         var args : string
-        
-#         for i in 2..<t.getType.len:
-#             args &= &"{repr t.getType[i]} "
-
-#         result[1].add newNimNode(nnkTupleConstr).add(
-#             newLit(&"{s} {args}"), newLit(&"{s} {res} {args}"))
-
-#     of nnkOpenSymChoice, nnkClosedSymChoice:
-#         for x in t:
-#             let res = repr x.getType[1]
-#             var args : string
-
-#             for i in 2..<x.getType.len:
-#                 args &= &"{repr x.getType[i]} "
-
-#             result[1].add newNimNode(nnkTupleConstr).add(
-#                 newLit(&"{s} {args}"), newLit(&"{s} {res} {args}"))
-#     else:
-#         error("Did not get a proc, probably a bug", t)
-
-# macro retreieveProc(desc : seq[string]) : untyped =
-#     result = newNimNode(nnkCall).add(
-#         newNimNode(nnkCast).add(
-#             newNimNode(nnkProcTy).add(
-#                 newNimNode(nnkFormalParams).add(
-#                     ident($desc[0])
-#                 ), newNimNode(nnkPragma).add(ident("nimcall"))
-#             )
-#         )
-#     )
-
-# var fnTable : Table[string, pointer] = toTable(
-#     bldFuncTable(hvMinus, "-") &
-#     bldFuncTable(hvPlus, "+") &
-#     bldFuncTable(hvTimes, "*")
-# )
-
-# var retTable : Table[string, string] = toTable(
-#     bldRetTable(hvMinus, "-") &
-#     bldRetTable(hvPlus, "+") &
-#     bldRetTable(hvTimes, "*")
-# )
-
-func callMagicFunc(id : string, args : seq[HvVal]) : HvExpr | HvNum =
+func callMagicFunc(id : string, args : seq[HvVal]) : HvExpr =
     case id:
     of "+ HvNum HvNum ":
-        return hvPlus(args[0].nVal, args[1].nVal)
+        return hvPlus(args[0].nVal, args[0].nVal)
+    of "+ HvExpr HvNum ", "+ HvNum, HvExpr ":
+        if args[0].kind == Num:
+            return hvPlus(args[0].nVal, args[1].eVal)
+        else:
+            return hvPlus(args[0].eVal, args[1].nVal)
     of "- HvNum HvNum ":
         return hvMinus(args[0].nVal, args[1].nVal)
     of "* HvNum HvNum ":
         return hvTimes(args[0].nVal, args[1].nVal)
+    of "* HvNum HvExpr ", "* HvExpr HvNum ":
+        if args[0].kind == Num:
+            return hvTimes(args[0].nVal, args[1].eVal)
+        else:
+            return hvTimes(args[0].eVal, args[1].nVal)
+
+    else: raise newException(Defect, &"Undefined function {id}")
 
 
 #----------Tree Walking Code----------#
@@ -261,7 +220,7 @@ proc evalAST(rt : ASTNode) : (HvVal, HvType) =
         result[0] = makeval callMagicFunc(paramTypes, params)
         result[1] = result[0].kind
 
-var rt = ASTNode(kind : NkRt)
-parseExpr(rt, tokenize partFile readFile("./symbols.txt").splitLines[6])
+var rt = strParse readFile("./symbols.txt").splitLines[6]
 print rt
 print evalAST(rt[0])[0]
+echo likeTerms(strParse("6 * 2")[0], strParse("1 * 8")[0])
